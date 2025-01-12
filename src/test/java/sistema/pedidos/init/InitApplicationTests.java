af2b1d7d3b596b8eb518c693295e637252449fb1
kuatrar0 <nicolasquatraro@gmail.com>
2025-01-11 21:42:43 -0300

    First commit

---
+++ src/test/java/sistema/pedidos/init/InitApplicationTests.java
@@ -0,0 +1,13 @@
+package sistema.pedidos.init;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
